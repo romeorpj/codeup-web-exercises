1ddee60f7f93c0bd07479c910f051268b6779a78
Richard Jackson <rpjackson2@gmail.com>
2022-02-09 14:45:40 -0600

    updated while loop exercise to only return odd numbers of nums 8-50

---
+++ js/for_loops.js
@@ -0,0 +1,36 @@
+let showMultiplicationTable = ()=>{
+//    ***7s TABLE
+//    show 7X ? up to 10
+//    concatenate 7 * num
+//    log out the response
+//     for(let i = 0; i < 11; i++){
+//         console.log(`7 x ${i} = ${7*i}`)
+//     }
+
+//*** ISODDISEVEN FOR LOOP
+//    generate 10 random numbers between 20 and 200
+    //log whether the numbers are odd or even
+
+//         for(let i = 0; i < 11; i++){
+//             let min = Math.ceil(20);
+//             let max = Math.floor(200);
+//             let random = Math.floor(Math.random() * (max - min) + min);
+//             if(random % 2 === 0){
+//                 console.log(`${random} is even`)
+//             }else{
+//                 console.log(`${random} is odd`)
+//             }
+// }
+
+//***    1 -9 PYRAMID
+    for(let i = 1;i <10;i++){
+        for(let j = 1;j <10;j++){
+
+
+        }
+
+    }
+
+}
+showMultiplicationTable();
+
---
+++ js/whileLoopExercises.js
@@ -0,0 +1,7 @@
+let num = 7;
+while(num >= 7 && num < 50){
+    num++
+    if(num % 2 !== 0){
+        console.log(num)
+    }
+}
