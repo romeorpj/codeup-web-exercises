7f47aec4c12a71d12cf12872017555c55a943bb4
Richard Jackson <rpjackson2@gmail.com>
2022-02-10 15:48:47 -0600

    uploading iterating array exercises from the curriculum

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,11 @@
 // prompt user to input any odd number between 1 and 50
 
 let numPrompt = parseInt(prompt(`Please enter any odd number between and including 1 and 50`))
+
 while (numPrompt % 2 === 0 || numPrompt === "") {
     numPrompt = parseInt(prompt(`Please enter any odd number between and including 1 and 50`))
-
 }
+
 if (numPrompt % 2 !== 0) {
     console.log(`Number to skip is: ${numPrompt}`)
     for (let i = 1; i < 50; i++) {
