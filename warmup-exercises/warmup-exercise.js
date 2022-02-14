3069f79564903acfa0261f11877430d978876a96
Richard Jackson <rpjackson2@gmail.com>
2022-02-14 09:27:33 -0600

    added reduce method to todays warmup question

--- warmup-exercises/warmup-exercise.js
+++ warmup-exercises/warmup-exercise.js
@@ -3,12 +3,17 @@
 // average.
 //     Example input: [95, 74, 86, 100]
 // Example output: 88.75 (edited)
-
-(function findAverage()  {
+// created using for i. and an option using reduce
+(function findAverage() {
     let arr = [95, 74, 86, 100];
-    let numHolder = 0;
-    for(let i of arr){
-        numHolder += i;
-    }
-    console.log(numHolder /arr.length)
+    // let numHolder = 0;
+    // for (let i of arr) {
+    //     numHolder += i;
+    // }
+    // console.log(numHolder / arr.length)
+
+    let newReduce = arr.reduce((acc,curr)=>{
+        return acc + curr;
+    },0)
+console.log(newReduce / arr.length)
 })();
