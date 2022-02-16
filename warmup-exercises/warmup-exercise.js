95118df64fa635b0a6106ca5313a9c5004d7e899
Richard Jackson <rpjackson2@gmail.com>
2022-02-16 10:46:14 -0600

    finished new object exercise

--- warmup-exercises/warmup-exercise.js
+++ warmup-exercises/warmup-exercise.js
@@ -25,28 +25,37 @@
 // occupation
 // Hard code some values in that object, then log it and its properties to the console
 
-let person = {
-    names: ["Richard", "Jackson"],
-    dob: "11/09/1985",
-    occupation: "Software Developer"
-}
 
-// Create an object representing a person’s contact info. It should have:
-//     phone number
-// street address
-// state
-// postal code
-// email address
-// Hard code some values in that object, then log it and its properties to the console
 
-let personsContactInfo = {
-    phoneNumber: "333-333-7777",
-    address: "1114 some street",
-    city: "San Antonio",
-    state: "TX",
-    postal_code: "77232",
-    email: "myemail@gmail.com"
+
+// Defines a contact info object
+function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
+    this.phoneNumber = phoneNumber;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.emailAddress = emailAddress;
 }
-console.log(`${personsContactInfo.address}
-${personsContactInfo.state}, ${personsContactInfo.city} ${personsContactInfo.postal_code}`);
+// Defines a person object
+function Person(names, occupation, dob, contactInfo) {
+    this.names = names;
+    this.occupation = occupation;
+    this.dob = dob;
+    this.contactInfo = contactInfo;
+}
+// makes a new person object instance (by invoking the constructor)
+let person2 = new Person(
+    ["Richard", "Jackson"],
+    "11/09/1985",
+    "Software Developer",
+    new ContactInfo(
+        "333-333-7777",
+        "1114 some street",
+        "San Antonio",
+        "TX",
+        "77232",
+        "myemail@gmail.com"));
+
+console.log(person2.contactInfo.emailAddress);
 
