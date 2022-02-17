40b4aee169c85fa0423ac65922828bfd030c08c2
Richard Jackson <rpjackson2@gmail.com>
2022-02-17 10:14:34 -0600

    added web exercise

--- warmup-exercises/js/warmup-exercise.js
+++ warmup-exercises/js/warmup-exercise.js
@@ -26,8 +26,6 @@
 // Hard code some values in that object, then log it and its properties to the console
 
 
-
-
 // Defines a contact info object
 function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
     this.phoneNumber = phoneNumber;
@@ -58,4 +56,28 @@ let person2 = new Person(
         "myemail@gmail.com"));
 
 console.log(person2.contactInfo.emailAddress);
+
+// Warmup part 4
+
+// We are going to create a method which
+//  allows a user to input their desired amount of names names
+//      use prompt, alert, confirm inside of a while loop
+//     assign each name to an array
+//  once the user decides they are done giving names, end the loop and return the string array
+//  then assigning that returned array by invoking your new function on the Person.names property value!
+//     Things to consider:
+//      Should this be a standalone function or should it be a method on the Person object?
+
+function enterNames() {
+    let nameArr = [];
+    do {
+        let askForName = prompt(`Enter New name `);
+        alert(`You entered ${askForName}`)
+        nameArr.push(askForName)
+    }
+    while (confirm(`Would you like to enter another name?`))
+    return nameArr;
+}
+
+enterNames();
 
