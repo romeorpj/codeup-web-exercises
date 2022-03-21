a0b2494aa49990d62faf5d8c2e770541c7243269
Richard Jackson <rpjackson2@gmail.com>
2022-03-21 17:18:50 -0500

    working on style and minor function changes

--- js/mapbox-weather.js
+++ js/mapbox-weather.js
@@ -2,17 +2,20 @@
 let input1 = document.querySelector("#input1");
 let btn1 = document.querySelector(".header-btn1");
 let mapStuffSection = document.querySelector("#map-stuff");
+let reverseCloseBtn = document.querySelector(".fa-circle-xmark");
+let reverseGeoWrapper1 = document.querySelector(".reverseGeoWrapper");
+let marker= new mapboxgl.Marker();
 // console.log(body)
 //global map for access by input1 event
 let map;
-let geocodeCoords;
+
 
 function setupMap(center){
 //    this function sets the starting details of the map
 mapboxgl.accessToken = MAPBOX_KEY;
 map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
     // center coords are pulled from wather-map geolocation
     center: center, // starting position [lng, lat]
     zoom: 16, // starting zoom
@@ -21,25 +24,50 @@ trackResize: true,
 // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl(),'bottom-right');
 
+    // markerInfo = {
+    //     address: "",
+    //     popupHTML: "<p>Remember the Alamo!</p>"
+    // };
+
+
+    // console.log()
+    // placeMarkerAndPopup(markerInfo, MAPBOX_KEY, map);
+
+// ***TODO: REVERSE GEOCODING
     //Reverse Gecode onCLICK
 // language=HTML
     map.on('click', (e) => {
         let coordinates = {lng:e.lngLat.lng, lat:e.lngLat.lat}
         reverseGeocode(coordinates, MAPBOX_KEY).then((results)=>{
-            mapStuffSection.innerHTML = `
-                <div class="reverseGeoWrapper">
-                    <h2>Hi, you reverse searched for:</h2>
-                    <p>${results}</p>
-                </div>
-               
-            `;
+            // reverseGeoWrapper1.append(`${results}`);
+
+
+            getMapboxWeather(coordinates.lat, coordinates.lng).then(mapWeatherData => {
+                outputMapboxWeather(mapWeatherData)
+            }).catch(error => {
+                console.log(`This is probably somewhat kind of maybe not a good thing: ${error}`)
+            })
+placeMarkerAndPopup(results, MAPBOX_KEY, map);
             console.log(`this is results reverse ${results}`)
         })
-        console.log(e)
-        console.log(e.lngLat.lng)
-        console.log(`A click event has occurred at ${e.lngLat}`);
+        // console.log(e)
+        // console.log(e.lngLat.lng)
+        // console.log(`A click event has occurred at ${e.lngLat}`);
     });
-
+}
+//END MAP OBJECT
+//TODO: MARKERS & POPUP
+function placeMarkerAndPopup(info, token, map) {
+    geocode(info, token).then(function(coordinates) {
+        let popup = new mapboxgl.Popup()
+            .setHTML(`<h2>reverse search results:</h2>
+${info}`);
+        marker
+            .setLngLat(coordinates);
+            marker.addTo(map)
+            .setPopup(popup);
+        popup.addTo(map);
+    });
 }
 //TODO: GEOCODING
 btn1.addEventListener("click",(e)=>{
@@ -58,15 +86,18 @@ let geoSearch = input1.value;
 });
 
 // TODO: getWeatherData calls the openweather API *** TO UPDATE THE FORWARD GEOCODE WEATHER
-async function getMapboxWeather(result) {
-    let [mapLong, mapLat] = result;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${mapLat}&lon=${mapLong}&&units=imperial&exclude=current,hourly,minutely&appid=${OWM_KEY}`)
+async function getMapboxWeather(lat, long){
+
+
+    // let [mapLong, mapLat] = result;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&&units=imperial&exclude=current,hourly,minutely&appid=${OWM_KEY}`)
     if (!response.ok) {
         new Error(`HTTP error! status: ${response.status}`)
     } else {
         return await response.json();
     }
 }
+
 
 function outputMapboxWeather(mapWeatherData){
     forecastScroller.innerHTML = "";
@@ -77,7 +108,7 @@ function outputMapboxWeather(mapWeatherData){
           <div class="date-container flex-row">
               <img src="http://openweathermap.org/img/wn/${mapWeatherData.daily[i].weather[0].icon}@2x.png" class="owm-icon">
        <p class="forecast-day">
-           ${new Date(mapWeatherData.daily[i].dt).toLocaleString('en-us', {weekday: 'short'})}
+           ${new Date(mapWeatherData.daily[i].dt * 1000).toLocaleString('en-us', {weekday: 'short'})}
            </p>
        </div>
        <div class="forecast-hilo flex-row">
@@ -99,11 +130,4 @@ function outputMapboxWeather(mapWeatherData){
          </div>
      `
     }
-
 }
-
-
-// ***TODO: REVERSE GEOCODING
-
-// Set an event listener on the map
-
--- js/weather-map.js
+++ js/weather-map.js
@@ -30,7 +30,7 @@ function successLocation(position) {
         .then((dataArr) => {
 
             outputOWM(dataArr)
-            console.log(dataArr)
+            console.log(new Date(dataArr.daily[2].dt * 1000).toLocaleString('en-us', {weekday: 'short'}))
             //  console.log(weatherArrSwitcher.daily[1].weather[0].icon)
         }).catch(error => {
         console.log(`This is probably somewhat kind of maybe not a good thing: ${error}`)
@@ -49,9 +49,10 @@ function outputOWM(dataArr) {
 // if(forecastScroller){
 //     forecastScroller.innerhtml="";
 // }else{
-    console.log(`this is weatherArrSwitcher html call ${dataArr.daily[0]}`)
+//     console.log(`this is weatherArrSwitcher html call ${dataArr.daily[0]}`)
+//     console.log(dataArr.daily[i].dt)
 
-
+let daysOfWeek = ["Sun", "Mon","Tue", "Wed","Thur", "Fri","Sat"]
     for (let i = 0; i <= 6; i++) {
         forecastScroller.innerHTML += `
             <div class="forecast flex-col">
@@ -59,24 +60,24 @@ function outputOWM(dataArr) {
                     <img src="http://openweathermap.org/img/wn/${dataArr.daily[i].weather[0].icon}@2x.png"
                          class="owm-icon">
                     <p class="forecast-day">
-                        ${new Date(dataArr.daily[i].dt).toLocaleString('en-us', {weekday: 'short'})}
+                        ${new Date(dataArr.daily[i].dt * 1000).toLocaleString('en-us', {weekday: 'short'})}
                     </p>
                 </div>
                 <div class="forecast-hilo flex-row">
-                    <p class="forecast-lo">Lo: ${dataArr.daily[i].temp.min} /</p>
-                    <p class="forecast-hi">Hi: ${dataArr.daily[i].temp.max}</p>
+                    <p class="forecast-lo">Lo: <span class="hilo-temp">${dataArr.daily[i].temp.min}</span></p>
+                    <p class="forecast-hi">Hi: <span class="hilo-temp">${dataArr.daily[i].temp.max}</span></p>
                 </div>
-                <div class="description-wrapper flex-col">
-                    <p class="description-title">Description:</p>
+                <div class="description-wrapper flex-row">
+                    
                     <p class="description-output">${dataArr.daily[i].weather[0].description}</p>
                 </div>
                 <div class="humidity-wrapper flex-row">
                     <p class="humidity-title">Humidity:</p>
-                    <p class="humidity-output">${dataArr.daily[i].humidity}</p>
+                    <p class="humidity-output"> ${dataArr.daily[i].humidity}</p>
                 </div>
                 <div class="wind-speed-wrapper flex-row">
                     <p class="wind-speed-title">Wind Speed:</p>
-                    <p class="wind-speed-output">${dataArr.daily[i].wind_speed}</p>
+                    <p class="wind-speed-output">    ${dataArr.daily[i].wind_speed}</p>
                 </div>
             </div>
         `
